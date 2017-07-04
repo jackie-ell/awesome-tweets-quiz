f47d390ae3fe7e10a7ec393b8f49cf7788e81b99
Jackie Ellenberger <jackie.ellenberger@gmail.com>
2017-07-04 13:42:08 -0700

    Add new tweet functionality

--- app.js
+++ app.js
@@ -13,12 +13,39 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(cookieParser());
 
+app.use((req, res, next) => {
+  const {tweets} = req.cookies;
+
+  res.locals.tweets = tweets;
+
+  console.log(res.locals.tweets);
+  next();
+});
+
+// URL: http://localhost:4545 HTTP VERB: GET
 app.get('/', (req, res) => {
   res.render('index');
 });
 
+// URL: http://localhost/dash:4545 HTTP VERB: GET
 app.get('/dash', (req,res) => {
   res.render('dash');
+});
+
+// URL: http://localhost/dash:4545 HTTP VERB: POST
+app.post('/dash', (req, res) => {
+  const {username} = req.body;
+  const {newEntry} = req.body;
+  let tweetArr = [];
+  if(req.cookies.tweets){
+    tweetArr = req.cookies.tweets;
+  }
+
+  tweetArr.push([username, newEntry, new Date().toDateString()]);
+  console.log(tweetArr);
+  res.cookie('tweets', tweetArr, {maxAge: 1000*60*60*24});
+
+  res.redirect('dash');
 });
 
 
